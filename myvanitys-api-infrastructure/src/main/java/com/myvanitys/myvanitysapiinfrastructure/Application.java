d1de040235bc2a4676817ff390d91e01c2095a6c
kennyjpc <kennyjpc@ext.inditex.com>
2024-12-27 12:37:35 +0100

    VANITYS01: Rename main project class

--- myvanitys-api-infrastructure/src/main/java/com/myvanitys/myvanitysapiinfrastructure/Application.java
+++ myvanitys-api-infrastructure/src/main/java/com/myvanitys/myvanitysapiinfrastructure/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyvanitysApiInfrastructureApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyvanitysApiInfrastructureApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
