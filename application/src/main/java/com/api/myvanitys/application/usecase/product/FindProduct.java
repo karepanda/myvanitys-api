81984d6d134318bb18b84752c3f713bd695b4401
kennyjpc <kennyjpc@ext.inditex.com>
2025-02-12 21:23:43 +0100

    VANITYS03: Fixed product use case

--- application/src/main/java/com/api/myvanitys/application/usecase/product/FindProduct.java
+++ application/src/main/java/com/api/myvanitys/application/usecase/product/FindProduct.java
@@ -1,29 +1,30 @@
 package com.api.myvanitys.application.usecase.product;
 
+import java.util.Optional;
+
 import com.api.myvanitys.application.query.FindProductQuery;
 import com.api.myvanitys.domain.model.Product;
+import com.myvanitys.infrastructure.adapters.outbound.persistence.entity.ProductEntity;
 import com.myvanitys.infrastructure.adapters.outbound.persistence.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class FindProduct {
 
-  private final ProductRepository productRepository;
+  private final ProductRepository productRepository; // Inject repository
 
-  private final ProductMapper productMapper;  // Agregamos el mapper
-
-  public FindProduct(ProductRepository productRepository, ProductMapper productMapper) {
-    this.productRepository = productRepository;
-    this.productMapper = productMapper;
-  }
+  private final ProductMapper productMapper;  // Inject mapper
 
   public Product execute(FindProductQuery query) {
-        /*Optional<ProductEntity> productEntity = productRepository.findByName(query.getSearchTerms());
+    Optional<ProductEntity> productEntity = productRepository.findByName(query.getSearchTerms());
 
-        if (productEntity.isEmpty()) {
-            productEntity = productRepository.findByBrand(query.getSearchTerms());
-        }
+    if (productEntity.isEmpty()) {
+      productEntity = productRepository.findByBrand(query.getSearchTerms());
+    }
 
-        return productEntity.map(productMapper::toProduct) // Mapeamos ProductEntity -> Product
-                .orElse(null); // Retorna null si no encuentra nada*/
-    return null;
+    return productEntity.map(productMapper::toProduct) // Mapper ProductEntity -> Product (Domain ob)
+        .orElse(null); // return null if product not found*/
   }
 }
