e147f652f559a7d3042d33ac8bf7e8bc154735d5
kennyjpc <kennyjpc@ext.inditex.com>
2025-02-18 20:17:17 +0100

    HOTFIX: Fixed merge

--- domain/src/main/java/com/api/myvanitys/domain/model/Product.java
+++ domain/src/main/java/com/api/myvanitys/domain/model/Product.java
@@ -1,10 +1,16 @@
 package com.api.myvanitys.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import com.api.myvanitys.domain.valueobject.EntityId;
+import lombok.Getter;
 import lombok.NonNull;
+import lombok.ToString;
 
+@Getter
+@ToString
 public class Product {
 
   private final EntityId id;
@@ -12,6 +18,9 @@ public class Product {
   private final String brand;
   private final Category category;
   private final String colorHex;
+  private int averageRating;
+
+  private final List<Review> reviews = new ArrayList<>();
 
   public Product(EntityId id, @NonNull String name, @NonNull String brand, @NonNull Category category, @NonNull String colorHex) {
 
@@ -20,21 +29,40 @@ public class Product {
     this.brand = brand;
     this.category = category;
     this.colorHex = colorHex;
+    this.averageRating = 0;
+  }
+
+  public int getAverageRating() {
+    if (reviews.isEmpty()) {
+      return 0;
+    }
+
+    int totalRating = 0;
+    for (Review review : reviews) {
+      totalRating += review.getRating();
+    }
+
+    return totalRating / reviews.size();
+  }
+
+  public void addReview(@NonNull Review review) {
+    if (!reviews.contains(review)) {
+      reviews.add(review);
+    }
+  }
+  public void removeReview(Review review) {
+    if (reviews.remove(review)) {
+      calculateAverageRating();
+    }
+  }
+
+  private void calculateAverageRating() {
+    averageRating = reviews.isEmpty() ? 0 :
+            reviews.stream().mapToInt(Review::getRating).sum() / reviews.size();
   }
 
   @Override
   public int hashCode() {
     return Objects.hash(id);
-  }
-
-  @Override
-  public String toString() {
-    return "Product{" +
-            "id=" + id +
-            ", name='" + name + '\'' +
-            ", brand='" + brand + '\'' +
-            ", category=" + category +
-            ", colorHex='" + colorHex + '\'' +
-            '}';
   }
 }
